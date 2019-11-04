281139bf6ef4cefcdc38cee22c4d6092cbc31a23
mamun417 <abdullah.ssc13@gmail.com>
2019-11-04 22:49:27 +0600

    register page templating with custom layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -30,9 +30,9 @@
                                     <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                     @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
                                     @enderror
                                 </div>
                             </div>
@@ -44,9 +44,9 @@
                                     <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                     @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
                                     @enderror
                                 </div>
                             </div>
--- resources/views/frontend/layout/app.blade.php
+++ resources/views/frontend/layout/app.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="UTF-8">
 
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <title>@yield('title', 'Blog') - {{ config('app.name', 'Laravel blog site') }}</title>
 
     <!-- Font -->
@@ -12,8 +15,8 @@
 
     <!-- Stylesheets -->
     <link href="{{ asset('frontend/common-css/bootstrap.css') }}" rel="stylesheet">
-
     <link href="{{ asset('frontend/common-css/ionicons.css') }}" rel="stylesheet">
+    <link href="{{ asset('frontend/common-css/custom_style.css') }}" rel="stylesheet">
 
     @stack('css')
 
