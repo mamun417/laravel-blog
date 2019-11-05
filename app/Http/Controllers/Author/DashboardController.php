8e474450f35af411e84e8c5ab9a05062cb204c10
mamun417 <abdullah.ssc13@gmail.com>
2019-11-05 23:52:05 +0600

    author panel templating done

--- app/Http/Controllers/Author/DashboardController.php
+++ app/Http/Controllers/Author/DashboardController.php
@@ -8,6 +8,6 @@ use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
     public function index(){
-        return view('author.dashboard');
+        return view('backend.author.dashboard');
     }
 }
--- resources/views/backend/element/sidebar.blade.php
+++ resources/views/backend/element/sidebar.blade.php
@@ -2,12 +2,14 @@
     <div class="sidebar-collapse">
         <ul class="nav metismenu" id="side-menu">
             <li class="nav-header">
-                <div class="dropdown profile-element"> <span>
-                            <img alt="image" class="img-circle" src=" {{ asset('backend/img/profile_small.jpg') }}" />
-                             </span>
+                <div class="dropdown profile-element">
+                    <span>
+                        <img alt="image" class="img-circle" src=" {{ asset('backend/img/profile_small.jpg') }}" />
+                     </span>
                     <a data-toggle="dropdown" class="dropdown-toggle" href="#">
-                            <span class="clear"> <span class="block m-t-xs"> <strong class="font-bold">David Williams</strong>
-                             </span> <span class="text-muted text-xs block">Art Director <b class="caret"></b></span> </span> </a>
+                        <span class="clear"> <span class="block m-t-xs"> <strong class="font-bold">{{ Auth::user()->name }}</strong> </span>
+                        <span class="text-muted text-xs block">Art Director <b class="caret"></b></span> </span>
+                    </a>
                     <ul class="dropdown-menu animated fadeInRight m-t-xs">
                         <li><a href="profile.html">Profile</a></li>
                         <li><a href="contacts.html">Contacts</a></li>
@@ -28,9 +30,22 @@
                     IN+
                 </div>
             </li>
-            <li>
-                <a href="{{ route('admin.dashboard') }}"><i class="fa fa-dashboard"></i> <span class="nav-label">Dashboard</span></a>
-            </li>
+
+            @if(Request::is('admin*'))
+
+                <li>
+                    <a href="{{ route('admin.dashboard') }}"><i class="fa fa-dashboard"></i> <span class="nav-label">Dashboard</span></a>
+                </li>
+
+            @elseif(Request::is('author*'))
+
+                <li>
+                    <a href="{{ route('author.dashboard') }}"><i class="fa fa-dashboard"></i> <span class="nav-label">Dashboard</span></a>
+                </li>
+
+            @endif
+
+
         </ul>
 
     </div>
