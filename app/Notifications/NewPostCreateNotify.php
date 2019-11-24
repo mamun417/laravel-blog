de98a7939d240f6855bc1b5f8cf6a80a9213038f
Abdullah al mamun <aalmamun417@gmail.com>
2019-11-24 21:44:12 +0600

    send email by queue

--- app/Notifications/NewPostCreateNotify.php
+++ app/Notifications/NewPostCreateNotify.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class NewPostCreateNotify extends Notification
+class NewPostCreateNotify extends Notification implements ShouldQueue
 {
     use Queueable;
 
